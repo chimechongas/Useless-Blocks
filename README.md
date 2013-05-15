cf3e82a49135a8277e5308de954802c77ace9a41
OpenShift guest <mrchimechongas@gmail.com>
2013-05-15 19:07:50 -0400

    Added main mod file.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Useless-Blocks
 ==============
 
 An upcoming mod for tools for useless blocks.
+List of new things added:
+-Obsidian tools 
+-Emerald tools 
+-Sponge tools 
+-Brick tools 
+-Stone golem
---
+++ mod_UselessBlocks.java
@@ -0,0 +1,41 @@
+package net.minecraft.src;
+
+public class mod_UselessBlocks extends BaseMod{
+    
+    public static final Item ObsidianPickaxe = new ItemObsidianPickaxe(2107, EnumToolMaterial.EMERALD).setUnlocalizedName("Obsidianpickaxe.png");
+    public static final Item Spongepickaxe = new ItemSpongePickaxe(2112, EnumToolMaterial.WOOD).setUnlocalizedName("Spongepickaxe.png");
+    public static final Item EmeraldPickaxe = new ItemEmeraldPickaxe(2113, EnumToolMaterial.EMERALD).setUnlocalizedName("Emeraldpickaxe.png");
+    public static final Item BrickPickaxe = new ItemBrickPickaxe(2123, EnumToolMaterial.STONE).setUnlocalizedName("Brickpickaxe.png");
+    
+    public static final Item ObsidianShovel = new ItemObsidianShovel(2108, EnumToolMaterial.EMERALD).setUnlocalizedName("Obsidianshovel.png");
+    public static final Item SpongeShovel = new ItemSpongeShovel(2114, EnumToolMaterial.WOOD).setUnlocalizedName("Spongeshovel.png");
+    public static final Item EmeraldShovel = new ItemEmeraldShovel(2115, EnumToolMaterial.EMERALD).setUnlocalizedName("Emeraldshovel.png");
+    public static final Item BrickShovel = new ItemBrickShovel(2123, EnumToolMaterial.Stone).setUnlocalizedName("Brickshovel.png");
+    
+    public static final Item ObsidianHoe = new ItemObsidianHoe(2109, EnumToolMaterial.EMERALD).setUnlocalizedName("Obsidianhoe.png");
+    public static final Item SpongeHoe  = new ItemSpongeHoe(2116, EnumToolMaterial.WOOD).setUnlocalizedName("Spongehoe.png");
+    public static final Item EmeraldHoe = new ItemEmeraldHoe(2117, EnumToolMaterial.EMERALD).setUnlocalizedName("Emeraldhoe.png");
+    public static final Item BrickHoe = new ItemBrickHoe(2124, EnumToolMaterial.STONE).setUnlocalizedName("BrickHoe.png");
+    
+    public static final Item ObsidianSword = new ItemObsidianSword(2110, EnumToolMaterial.EMERALD).setUnlocalizedName("ObsidianSword.png");
+    public static final Item SpongeSword = new ItemSpongeSword(2118, EnumToolMaterial.WOOD).setUnlocalizedName("Spongesword.png");
+    public static final Item Emeraldsword = new ItemEmeraldSword(2119, EnumToolMaterial.EMERALD).setUnlocalizedName("EmeraldSword.png");
+    public static final Item BrickSword = new ItemBrickSword(2125, EnumToolMaterial.STONE).setUnlocalized
+
+    public static final Item ObsidianAxe = new ItemObsidianAxe(2111, EnumToolMaterial.EMERALD).setUnlocalizedName("Obsidianaxe.png");
+    public static final Item SpongeAxe = new ItemSpongeAxe(2120, EnumToolMaterial.WOOD).setUnlocalizedName("Spongesword.png");
+    public static final Item EmeraldAxe = new ItemEmeraldAxe(2121, EnumToolMaterial.EMERALD).setUnlocalizedName("Emeraldsword.png");
+    
+    public static final Item Troll = new ItemTroll(2122).setUnlocalizedName("Troll.png");
+
+    
+    public void load(){
+        
+        
+    }
+    
+    public String getVersion(){
+        return "0.1"
+    }
+    
+    }
